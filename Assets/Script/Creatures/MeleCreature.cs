2d670e13269bca8632195df354c69f0022b48344
tomhoyo <tom.hoyo@viacesi.fr>
2025-07-14 17:46:18 +0200

    add nexus

--- Assets/Script/Creatures/MeleCreature.cs
+++ Assets/Script/Creatures/MeleCreature.cs
@@ -1,5 +1,7 @@
 ﻿
 using System.Collections;
+using System.ComponentModel;
+using UnityEngine;
 
 namespace Assets.Script.Creatures
 {
@@ -8,25 +10,67 @@ namespace Assets.Script.Creatures
         public int move; 
         public int health; 
         public int attack; 
-        public int range; 
+        public int range;
+
+        private int[] goal = { 7, 0 };
+        private int[] direction = { 1, -1 };
+        private int[] baseToAttack = { 1, 0 };
 
         public override void Start()
         {
+            base.Start();
             pm = move;
-            hp = health;
-            atk = attack;
+            UpdateHP(health);
+            UpdateATK(attack);
             rng = range;
         }
 
-        public override IEnumerator Fight(Tile nextTile)
+        public override IEnumerator MoveForward(Lane lane)
         {
+            for (int i = 0; i < pm && (tile.pos[tile.tileIndex] != goal[team]) && this.alive; i++)
+            {
+                Tile nextTile = lane.tiles[tile.pos[tile.tileIndex] + direction[team]];
+                if (nextTile.creature == null)
+                {
+                    updateTile(nextTile);
+                    yield return MoveToNextTile(nextTile);
+                }
+                else
+                {
+                    yield return Fight(nextTile);
+                }
 
-            Creature ennemy = nextTile.creature;            
-            ennemy.hp -= this.atk;
-            this.hp -= ennemy.atk;
-            ennemy.CheckDead();
-            this.CheckDead();
+            }
 
+            if (tile.pos[tile.tileIndex] != goal[team] && lane.tiles[tile.pos[tile.tileIndex] + direction[team]].creature != null && this.alive)
+            {
+                yield return Fight(lane.tiles[tile.pos[tile.tileIndex] + direction[team]]);
+            }else if (tile.pos[tile.tileIndex] == goal[team] && this.alive)
+            {
+                yield return Fight(lane.baseTiles[baseToAttack[team]]);
+            }
+
+        }
+
+        public IEnumerator MoveToNextTile(Tile nextTile)
+        {
+            float speed = 5.0f;
+            float step = speed * Time.deltaTime;
+            while (transform.position != nextTile.transform.position)
+            {
+                transform.position = UnityEngine.Vector2.MoveTowards(transform.position, nextTile.transform.position, step);
+                yield return null;
+
+            }
+        }
+
+        public IEnumerator Fight(Tile nextTile)
+        {
+            Creature ennemy = nextTile.creature;
+
+            ennemy.UpdateHP(ennemy.hp - this.atk);
+            this.UpdateHP(this.hp - ennemy.atk);
+            
             yield return null;
         }
 
--- Assets/Script/Lane.cs
+++ Assets/Script/Lane.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Assets.Script
@@ -7,5 +8,32 @@ namespace Assets.Script
     {
         [SerializeField]
         public List<Tile> tiles;
+        [SerializeField]
+        public List<Tile> baseTiles;
+        public int playerBaseIndex { get; private set; } = 0;
+        public int ennemyBaseIndex { get; private set; } = 1;
+
+        [SerializeField]
+        private Creature basePrefab;
+        [SerializeField]
+        public List<Creature> bases;
+
+        private void Start()
+        {
+            InvokBase(playerBaseIndex);
+            InvokBase(ennemyBaseIndex);
+        }
+
+        private void InvokBase(int team)
+        {
+            Creature newBase = Instantiate(basePrefab, transform);
+
+            newBase.GetComponent<Creature>().team = team;
+            newBase.updateTile(baseTiles[team]);
+            newBase.transform.position = baseTiles[team].transform.position;
+
+            bases.Add(newBase);
+        }
+
     }
 }
